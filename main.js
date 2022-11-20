06288377ce2775d814cba1737e0671d811cadcad
AstroMT <81778387+AstroMT@users.noreply.github.com>
2022-11-20 15:07:25 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+var status = "";
+objects = [];
+current_status = "";
+
 function preload() {}
 
-function status() {}
+function setup() {
+    canvas = createCanvas(1240, 336);
+    canvas.center();
+ 
+    video = createCapture(VIDEO);
+    video.size(1240, 336);
+    video.hide();
+ 
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
 
-function draw() {}
+function draw() {
+    image(video, 0, 0, 600, 300);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Your model is loaded!");
+    current_status = true;
+}
