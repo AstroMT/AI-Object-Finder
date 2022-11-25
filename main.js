f51d2b8de0f301d2ccdbec38466f3dfd5ecb6497
AstroMT <81778387+AstroMT@users.noreply.github.com>
2022-11-25 15:18:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-var status = "";
+var current_status = "";
+video = "";
 objects = [];
-current_status = "";
-
-function preload() {}
 
 function setup() {
-    canvas = createCanvas(800, 600);
+    canvas = createCanvas(500, 400);
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.size(800, 600);
     video.hide();
 }
 
+var input = "";
+
 function start() {
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status_label").innerHTML = "Detecting Objects";
+    input = document.getElementById("object_input").value;
+    if(input = "") {
+        document.getElementById("status_label").innerHTML = "Enter an input before starting";
+        document.getElementById("found_label").innerHTML = "Error";
+    } else {
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded());
+        console.log(input);
+        document.getElementById("status_label").innerHTML = "Status: Detecting Objects";    
+    }
 }
 
 function modelLoaded() {
@@ -23,21 +29,16 @@ function modelLoaded() {
     current_status = true;
 }
 
-function gotResults(results) {
-    if(results.length > 0) {
-        console.log(results);
-        objects = results;
-    }
-}
-
 function draw() {
-    image(video, 0, 0, 800, 600);
+    image(video, 0, 0, 500, 450);
+
     if (current_status != "") {
-        objectDetector.detect(video, gotResults);
+        objectDetector.detect(video, gotResult);
+        document.getElementById("status_label").innerHTML = "Status: Detection Finished";
+        input = document.getElementById("object_input").value;
+        console.log(input);
 
         for (i = 0; i < objects.length; i++) {
-            document.getElementById("status_label").innerHTML = "Status: Detection Complete";
-
             fill("red");
 
             percent = floor(objects[i].confidence * 100);
@@ -47,13 +48,21 @@ function draw() {
             stroke("red");
             rect(objects[i].width, objects[i].height, objects[i].x, objects[i].y);
 
-            if (objects[i].label == document.getElementByid("object_input").value) {
-                document.getElementById("found_label").innerHTML = "Specified object was detected";
+            if(objects[i].label == input) {
+                document.getElementById("found_label").innerHTML = "A " + input + " was found"
             } else {
-                document.getElementById("found_label").innerHTML = "Specified object was not detected";
+                document.getElementById("found_label").innerHTML = "A " + input + " was not found"
             }
-
-
         }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        current_status = true;
+        objects = results;
     }
 }
